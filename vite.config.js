e5ccf280813ac83079702069034d934dc241c834
Calyster <clementbly1102@gmail.com>
2025-08-14 01:34:41 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     vueDevTools(),
 
   ],
-      base: '/bingo-app',
+  base: '/bingo-app',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
